a3a7a87672cf9f00a0b396ca0c2c69e1bf5e1189
helloworld <844255243@qq.com>
2018-12-27 15:30:46 +0800

    新增插件

--- eureka-client/src/main/java/cn/cloudey/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/cloudey/eurekaclient/EurekaClientApplication.java
@@ -1,15 +1,18 @@
 package cn.cloudey.eurekaclient;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Slf4j
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
+        log.info("===服务注册中心启动start===");
     }
 
 }

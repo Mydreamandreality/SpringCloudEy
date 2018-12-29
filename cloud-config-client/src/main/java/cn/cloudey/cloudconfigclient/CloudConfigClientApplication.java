4d72b3e60e9488b901a225ed07934052fe6f0ddb
helloworld <844255243@qq.com>
2018-12-29 11:13:49 +0800

    增加消息总线配置

--- cloud-config-client/src/main/java/cn/cloudey/cloudconfigclient/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/cn/cloudey/cloudconfigclient/CloudConfigClientApplication.java
@@ -2,10 +2,12 @@ package cn.cloudey.cloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class CloudConfigClientApplication {
 
     public static void main(String[] args) {

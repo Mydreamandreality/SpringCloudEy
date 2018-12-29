646e343b91d06416ea4b1aa9aa4e9de03d9270fa
helloworld <844255243@qq.com>
2018-12-29 10:29:05 +0800

    优化分布式配置中心

--- cloud-config-client/src/main/java/cn/cloudey/cloudconfigclient/controller/ConfigClientController.java
+++ cloud-config-client/src/main/java/cn/cloudey/cloudconfigclient/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package cn.cloudey.cloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Varsion 1.0
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${foo}")
     String foo;
--- cloud-config-server/src/main/java/cn/cloudey/cloudconfigserver/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/cn/cloudey/cloudconfigserver/CloudConfigServerApplication.java
@@ -3,9 +3,11 @@ package cn.cloudey.cloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer     //开启配置服务功能
+@EnableEurekaClient     //开启服务注册
 public class CloudConfigServerApplication {
 
     public static void main(String[] args) {

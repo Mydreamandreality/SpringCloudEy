ba782aa9ccbfef6d52832af37c1f2bdb1f45231b
helloworld <844255243@qq.com>
2019-01-04 11:44:30 +0800

    新增断路器监控

--- eureka-client/src/main/java/cn/cloudey/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/cloudey/eurekaclient/EurekaClientApplication.java
@@ -3,10 +3,18 @@ package cn.cloudey.eurekaclient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrix  //开启断路器
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @Slf4j
 public class EurekaClientApplication {
 
